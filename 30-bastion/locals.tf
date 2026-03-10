27210023e3452e0222a72e19ffdd10d6ef0dc694
surendra-karamsetty <surendra3207@gmail.com>
2026-03-10 20:11:58 +0530

    commit changes

--- 30-bastion/locals.tf
+++ 30-bastion/locals.tf
@@ -1,7 +1,8 @@
 locals {
   ami_id = data.aws_ami.terraform.id
+   # public subnet in 1a AZ
   public_sub_ids = split (",", data.aws_ssm_parameter.public_sub_ids.value)[0]
-  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.vale
+  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
   common_tags = {
     name = var.project
     environment = var.environment
