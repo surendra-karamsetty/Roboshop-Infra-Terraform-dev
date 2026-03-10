053d8991d56e8a064b797897f97b8f56709e19e3
surendra-karamsetty <surendra3207@gmail.com>
2026-03-10 20:57:44 +0530

    commit changes

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   subnet_id = local.public_sub_ids
   vpc_security_group_ids = local.bastion_sg_id
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   tags = merge (
     {
@@ -10,4 +11,41 @@ resource "aws_instance" "bastion" {
     },
     local.common_tags,
   )
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "RoboshopDevBastion"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge (
+    {
+        name = "${var.project}-${var.environment}-bastion"
+    },
+    local.common_tags,
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.project}-${var.environment}-bastion"
+  role = aws_iam_role.bastion.name
 }
